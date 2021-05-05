251623fe06c5b25c382a3b5e83d0fe0a0f42d822
FranMessina <fm230499@gmail.com>
2021-05-05 20:31:52 -0300

    Agregado de estructura de carpetas y archivos html y app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(3000, () => {
+    console.log("Servidor Corriendo en puerto 3000");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+});
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
+});
