2db06c4e06a2c9ed0c54bce2a39734764368fbc4
FranMessina <fm230499@gmail.com>
2021-05-09 17:01:04 -0300

    Agrego mas secciones a la home page

--- app.js
+++ app.js
@@ -28,7 +28,3 @@ app.get("/productDetail", (req, res) => {
 app.get("/productCart", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
 });
-
-app.get("/indexv2", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/indexV2.html"));
-});
