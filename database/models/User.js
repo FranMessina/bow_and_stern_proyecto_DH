46b6af38de97b5c2ea9c3bb3bf1338994a8cc5d8
SilvinaD <silvinadonvito21@gmail.com>
2021-07-09 22:19:44 -0300

    Actualizacion a base de datos

---
+++ database/models/User.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Users';
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        fullName: {
+            type: dataTypes.STRING
+        },
+        email: {
+            type: dataTypes.STRING
+        },   
+        pass: {
+            type: dataTypes.STRING
+        }
+    };
+    let config = { 
+        tableName: 'Users', 
+        timestamps: false};
+
+    const User = sequelize.define(alias, cols, config);
+
+    return User
+
+};
