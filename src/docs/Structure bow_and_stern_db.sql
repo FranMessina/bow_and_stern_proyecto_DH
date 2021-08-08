f57cea780e6c27b25e1df1d83af003a10aef176f
matiasgiles <Matias1giles@gmail.com>
2021-08-08 16:31:27 -0300

    detalles

--- src/docs/Structure bow_and_stern_db.sql
+++ src/docs/Structure bow_and_stern_db.sql
@@ -44,11 +44,11 @@ CREATE TABLE IF NOT EXISTS `bow_and_stern_db`.`boats` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `short_description` VARCHAR(255) NOT NULL,
-  `image` TEXT NOT NULL,
+  `image` VARCHAR(255) NOT NULL,
   `year` INT NOT NULL,
   `measures` INT NOT NULL,
   `vessel_type` VARCHAR(255) NOT NULL,
-  `description` LONGTEXT NOT NULL,
+  `description` VARCHAR(255) NOT NULL,
   `locations_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_boats_locations_idx` (`locations_id` ASC))
@@ -58,6 +58,12 @@ CREATE TABLE IF NOT EXISTS `bow_and_stern_db`.`boats` (
   --  ON DELETE NO ACTION
   --  ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+ALTER TABLE `bow_and_stern_db`.`boats` 
+CHANGE COLUMN `image` `image` VARCHAR(255) NOT NULL DEFAULT '1628448723543.jpg' ;
+
+ALTER TABLE `bow_and_stern_db`.`boats` 
+CHANGE COLUMN `locations_id` `locations_id` INT(11) NOT NULL DEFAULT 1 ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
