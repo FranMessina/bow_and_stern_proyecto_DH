1e05cbacd25f08fad1af34a8ea68d6c111f89090
SilvinaD <silvinadonvito21@gmail.com>
2021-09-10 21:40:50 -0300

    Pequeños ajustes varios

--- public/js/editProduct.js
+++ public/js/editProduct.js
@@ -1,24 +1,23 @@
-
-    const form = document.querySelector('.create-form')
+const form = document.querySelector('.create-form')
     
-    // Los inputs
-    const inputName = form.querySelector('input[name=name]')
-    const inputYear = form.querySelector('input[name=year]')
-    const inputMeasure = form.querySelector('input[name=measures]')
-    const inputVesselType = form.querySelector('select[name=vesselType]')
+// Los inputs
+const inputName = form.querySelector('input[name=name]')
+const inputYear = form.querySelector('input[name=year]')
+const inputMeasure = form.querySelector('input[name=measures]')
+const inputVesselType = form.querySelector('select[name=vesselType]')
     
 const inputDesc = form.querySelector('#description')
 const inputShortDesc = form.querySelector('#shortDescription')
 const inputImages = form.querySelector('input[name=image]')
-    // Error msgs
-    const errorDisplay = form.querySelector('.error-display')
 
-    const currentTime = new Date()
+// Error msgs
+const errorDisplay = form.querySelector('.error-display')
+
+const currentTime = new Date()
     inputYear.setAttribute('max',currentTime.getFullYear())
     let errorName = ''
     let errorYear = ''
     let errorMeasures = ''
-
     
     let errorsMsgArray = [];
 
@@ -32,7 +31,7 @@ const displayErrors = () => {
      })
  }
 
- const resetErrors = () => {
+const resetErrors = () => {
     errorDisplay.innerHTML = ''
 }
 
@@ -69,12 +68,10 @@ const validate = (e) => {
         errorsMsgArray.push('Input a short description')
     }
 
-
     if (!inputVesselType.value) {
         hasErrors = true
         errorsMsgArray.push('Select you vessel category')
     }
-
 
     if(hasErrors) {
         displayErrors()
@@ -83,36 +80,6 @@ const validate = (e) => {
     }
 
 }
-
-
-   /* function formValidation (e){
-        let hasErrors = false
-        resetErrors()
-    
-        if (inputName.value.length < 3) {
-            hasErrors = true
-            errorName = "Name incorrect. Please, try again."
-        }
         
-        if (inputYear.value.length < 3) {
-            hasErrors = true
-            errorYear = "Year incorrect. Please, try again."
-        }
-
-        if (inputMeasure.value.length < 3) {
-            hasErrors = true
-            errorMeasures = "Feet incorrect. Please, try again."
-        }
-        
-        if (hasErrors) {
-          errorMsg.innerHTML = errorName + ' ' + errorYear + '' + inputMeasure
-          errorMsg.style.display='block'
-          errorMsg.style.color='red'
-          errorMsg.style.fontSize= "12px"
-          e.preventDefault()
-        } 
-    
-      }*/
-        
-    form.addEventListener("submit", validate)
+form.addEventListener("submit", validate)
         
