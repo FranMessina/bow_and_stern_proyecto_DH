d061cf97983633a85cdb4bf332b7d8bfce2e5038
FranMessina <fm230499@gmail.com>
2021-09-10 21:09:12 -0300

    Unos comentarios

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,32 +1,29 @@
-const path = require('path');
+const path = require("path");
 
-const db = require('../database/models');
+const db = require("../database/models");
 const Op = db.Sequelize.Op;
-
-
-
 
 const productController = {
 	productDetail: async (req, res) => {
 		const boat = await db.Boat.findByPk(req.params.id);
-			res.render('products/productDetail', {boat});
+		res.render("products/productDetail", { boat });
 	},
 	createListing: (req, res) => {
-			res.render('products/listingForm');
+		res.render("products/listingForm");
 	},
-	catalogue: async (req, res) => {	
-		const boats = await db.Boat.findAll( {
-			include: ['location']
-		})  
-		
-			res.render('products/catalogue', { boats });
-		
+	catalogue: async (req, res) => {
+		const boats = await db.Boat.findAll({
+			include: ["location"],
+		});
+
+		res.render("products/catalogue", { boats });
+
 		// le pasa la info de todos los botes del servidor a mi vista catalogue
 	},
 
-	edit: async (req, res) => {	
-		const boat = await db.Boat.findByPk(req.params.id) 
-			res.render('products/editProduct', { boat });
+	edit: async (req, res) => {
+		const boat = await db.Boat.findByPk(req.params.id);
+		res.render("products/editProduct", { boat });
 
 		// le pasa la info de todos los botes del servidor a mi vista catalogue
 	},
@@ -34,106 +31,81 @@ const productController = {
 		const boats = await db.Boat.findAll();
 		const user = req.session.logged;
 
-		if(user.role == 'admin'){
-            res.render('products/controlPanel', { boats });
-        }
-    
-        // lo que sigue se puede quitar - el nav ya no muestra la opcion para usuario no admin
-        else
-        {       
-            res.send('Not allowed, must be administrator');
+		if (user.role == "admin") {
+			res.render("products/controlPanel", { boats });
+		}
+
+		// lo que sigue se puede quitar - el nav ya no muestra la opcion para usuario no admin
+		// Osea como poder se puede, pero no deberia, porque alguien puede ingresar la URL manualmente y estaria bueno que lo rechaze,
+		// Aunque tal vez seria mejor que se lo redirija a otra pagina y ahi salte el mensaje de alguna manera.
+		else {
+			res.send("Not allowed, must be administrator");
 		}
 	},
 
 	create: async (req, res) => {
-		
 		//es req.file porque lo manda por mullter
 
 		const image = req.file.filename;
 
-		const {
-			name,
-			shortDescription,
-			year,
-			measures,
-			vesselType,
-			description,
-		} = req.body;
+		const { name, shortDescription, year, measures, vesselType, description } =
+			req.body;
 
-		await db.Boat.create(
-			{
-				name: name,
-				short_description: shortDescription,
-				year: year,
-				measures:measures,
-				vessel_type:vesselType,
-				description: description,
-				image: image,
-			}
-		)
-		res.redirect("/products/catalogue")
-
+		await db.Boat.create({
+			name: name,
+			short_description: shortDescription,
+			year: year,
+			measures: measures,
+			vessel_type: vesselType,
+			description: description,
+			image: image,
+		});
+		res.redirect("/products/catalogue");
 
 		//es req.body porque lo manda por el body del formulario.
-
-	
 	},
 
-	delete:  (req, res) => {
+	delete: (req, res) => {
 		//requiero el id del producto que fue selecccionado para eliminar
 		const id = req.params.id;
 		//ahora le paso ese id al modelo para que haga su magia
-		db.Boat.destroy(
-			{where :{id:id}}
-		);
+		db.Boat.destroy({ where: { id: id } });
 
 		// Por ultimo, lo redirijo a la pajina principal una vez que el archivo ya fue #destroyed
-		res.redirect('/products/catalogue');
+		res.redirect("/products/catalogue");
 	},
 
-
 	location: async (req, res) => {
+		const locationId = req.params.id;
+		const locationFormId = req.body.location;
 
-	const locationId = req.params.id
-	const locationFormId = req.body.location
+		if (locationId) {
+			const boats = await db.Boat.findAll({
+				where: {
+					locations_id: locationId,
+				},
+			});
+			res.render("products/catalogue", { boats });
+		}
+		if (locationFormId) {
+			const boats = await db.Boat.findAll({
+				where: {
+					"$location.location$": { [Op.substring]: locationFormId },
+				},
+				include: ["location"],
+			});
 
-	if (locationId) {
-		const boats = await db.Boat.findAll( {
-			
-			where: {
-				locations_id: locationId
-			}
-		})  
-		res.render('products/catalogue', { boats })
-	}
-	 if (locationFormId) {
+			res.render("products/catalogue", { boats });
+		}
 
-	 	const boats = await db.Boat.findAll( {
-	 		where: {
-				 "$location.location$": {[Op.substring]: locationFormId}
-			 },
-			 include: ["location"]
-	 		
-	 	})  
-	
-	 	res.render('products/catalogue', { boats })
-	 }
+		// if (locationFormId) {
 
-	// if (locationFormId) {
+		// 	const boats = await db.seq.query("select * from boats inner join locations on locations_id=locations.id where locations.location like" + locationFormId)
+		// 	res.render('products/catalogue', { boats })
+		// }
+	},
 
-	// 	const boats = await db.seq.query("select * from boats inner join locations on locations_id=locations.id where locations.location like" + locationFormId)
-	// 	res.render('products/catalogue', { boats })
-	// }
-
-
-
-	}
-	
-	
-	
-	,
 	// console.log(boats[0].location.location)
-
 
 	update: async (req, res) => {
 		const data = req.body;
@@ -155,29 +127,23 @@ const productController = {
 		}
 		data.image = image;
 
-		const {
-			name,
-			shortDescription,
-			year,
-			measures,
-			vesselType,
-			description,
-		} = req.body;
+		const { name, shortDescription, year, measures, vesselType, description } =
+			req.body;
 
 		await db.Boat.update(
 			{
 				name: name,
 				short_description: shortDescription,
 				year: year,
-				measures:measures,
-				vessel_type:vesselType,
+				measures: measures,
+				vessel_type: vesselType,
 				description: description,
 				image: image,
-			}, {where: {id: id}}
-		)
-			res.redirect('/products/detail/' + id);
-	}
-
+			},
+			{ where: { id: id } }
+		);
+		res.redirect("/products/detail/" + id);
+	},
 };
 
 module.exports = productController;
