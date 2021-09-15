cf2777506d0351726bb4dc0593e3ce17f813b6ac
matiasgiles <Matias1giles@gmail.com>
2021-09-14 22:52:33 -0300

    arreglando cagaditas

--- src/routes/api/boatApiRoutes.js
+++ src/routes/api/boatApiRoutes.js
@@ -1,20 +1,15 @@
-/*MATIAS
-const express = require('express');
 const productApiController = require ("../../controllers/api/productApiController")
-const boatApiRoutes = express.Router()
-
-boatApiRoutes.get("/search", productApiController.searchLocations)
-
-boatApiRoutes.get("/getBoat", productApiController.getBoat)
-
-
-module.exports= boatApiRoutes;
-*/
 const express = require("express");
 const boatApiRoutes = express.Router();
 const boatApiController = require ("../../controllers/api/boatApiController")
 
 //endpoints
 boatApiRoutes.get("/", boatApiController.listBoats)
-boatApiRoutes.get("/:id", boatApiController.selectBoat)
-module.exports= boatApiRoutes
+boatApiRoutes.get("/detail/:id", boatApiController.selectBoat)
+
+
+boatApiRoutes.get("/search", productApiController.searchLocations)
+
+
+
+module.exports= boatApiRoutes;
